63d5e68118cddcecb86903b9d69712e36b52484f
Yonnani <iamai5206@gmail.com>
2020-07-07 01:24:07 +0900

    Hello World

---
+++ react-native-cli-test/nativeTest/App.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.hello}>Hello World!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  hello: {
+    color: 'red'
+  }
+});
+
+export default App;
