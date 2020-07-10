602219581469e9dcfa05c9f5fdee672adc34f579
Yonnani <iamai5206@gmail.com>
2020-07-11 00:12:43 +0900

    width, height 실습

--- react-native-cli-test/nativeStyleEx/App.js
+++ react-native-cli-test/nativeStyleEx/App.js
@@ -45,6 +45,10 @@ class App extends Component {
                     </View>
                 </View>
             </View>
+
+            <View style={{width: 50, height: 50, backgroundColor: 'red'}}></View>
+            <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}}></View>
+            <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}}></View>
           </View>
       );
   }
